7c30dd06db7201b430e93c9d63bd44fbb337458c
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-04 20:22:33 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,6 @@ class SongsController < ApplicationController
   end
 
   def upload
-    binding.pry
     CSV.foreach(params[:file].path, headers: true) do |song|
       new_song = Song.new
       new_song.title = song[0]
